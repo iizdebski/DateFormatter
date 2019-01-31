beefec008dea854e9c9fcc5bb446fbe2b87a684a
Ivan Izdepskyi <iizdepskyy112@gmail.com>
2019-01-31 17:48:08 +0200

    Date Formatter

---
+++ src/com/izdebski/Test.java
@@ -0,0 +1,17 @@
+package com.izdebski;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        List<String> list = getList();
+        List<String> listOpt = list != null ? list : new ArrayList<>();
+    }
+
+    private static List<String> getList() {
+        return null;
+    }
+}
